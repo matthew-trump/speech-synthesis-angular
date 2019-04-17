ab54b7ee4d93734e1fcc96fa4f66bab637f91c49
matthew-trump <matthew.trump@gmail.com>
2019-04-17 09:18:12 -0700

    initial route for tts google

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GoogleTextToSpeechComponent } from './google-text-to-speech/google-text-to-speech.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'google-text-to-speech', pathMatch: 'full', component: GoogleTextToSpeechComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
